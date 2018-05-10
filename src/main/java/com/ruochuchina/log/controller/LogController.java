136e8592a12be90548bce04ee6ff753de8a831f6
nguyen <rwm@ruochuyizhu.com>
2018-05-11 01:47:11 +0800

    log

---
+++ src/main/java/com/ruochuchina/log/controller/LogController.java
@@ -0,0 +1,28 @@
+package com.ruochuchina.log.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author RWM
+ * @date 2018/4/23
+ */
+@RestController
+@RequestMapping("/logger")
+public class LogController {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+
+    @RequestMapping(path = "/log", method = RequestMethod.GET)
+    public void log(){
+        int j = 10_000;
+        for(int i=0;i<j;i++){
+            logger.info("info execute index method");
+            logger.warn("warn execute index method");
+            logger.error("error execute index method");
+        }
+    }
+}
